7c2ce61765a577db56b4bbc030a6a8919ba34703
Sano <sanasar.hambardzumyan@gmail.com>
2022-12-02 23:43:19 +0400

    Changes

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,25 @@
 package com.epam.mjc.nio;
 
-import java.io.File;
+import java.io.*;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String name = "";
+        int age = 0;
+        String email = "";
+        long phone = 0;
+        try (BufferedReader bf = new BufferedReader(new InputStreamReader(new FileInputStream(file)))) {
+           name = bf.readLine().substring(6);
+           age = Integer.parseInt(bf.readLine().substring(5));
+           email = bf.readLine().substring(7);
+           phone = Long.parseLong(bf.readLine().substring(7));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new Profile(name,age,email,phone);
     }
 }
